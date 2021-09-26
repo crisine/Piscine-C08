eb9c12756047fa4bbb25d5f94dba8878eb645f2c
misung <crisine@knou.ac.kr>
2021-09-26 17:14:58 +0900

    modified true, false in ex01

--- ex01/ft_boolean.h
+++ ex01/ft_boolean.h
@@ -6,7 +6,7 @@
 /*   By: misung <misung@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/23 11:08:21 by misung            #+#    #+#             */
-/*   Updated: 2021/09/23 11:56:22 by misung           ###   ########.fr       */
+/*   Updated: 2021/09/26 17:13:30 by misung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,11 @@
 
 typedef enum boolean
 {
-	true = 1,
-	false = 0
+	TRUE = 1,
+	FALSE = 0
 }	t_bool;
 
 # define EVEN(number) (number % 2 == 0)
-
-# define TRUE true;
-# define FALSE false;
 
 # define EVEN_MSG "I have an even number of arguments."
 # define ODD_MSG "I have an odd number of arguments."
